1b8909a4ec865aa45d5db6accb960cda89dcb6a4
rroyalty <rroyalty@gmail.com>
2021-01-27 21:53:10 -0500

    Code for Jon

--- assets/scripts/ryanJS.js
+++ assets/scripts/ryanJS.js
@@ -2,21 +2,36 @@ $(document).ready(function() {
     const drinkAPIkey = "1";
     const drinkURL = "https://www.thecocktaildb.com/api/json/v1/" + drinkAPIkey + "/";
 
-    let ingredients = ["Vodka", "Gin"];
+    // let ingredients = ["Vodka", "Gin"];
 
-    searchByIngredients(ingredients);
+    // searchByIngredients(ingredients);
 
-    console.log(ingredients);
+    // console.log(ingredients);
 
-    function searchByIngredients(array) {
-        let arrayArrays = [];
-        let workingURL = drinkURL + "filter.php/?i=";
-        for(i=0; i < array.length; i++) {
-            $.get(workingURL + array[i], function(response) {
-                arrayArrays[i] = array.map(value => value.key);
-                console.log(arrayArrays[i]);
-            })
-        }
+    // function searchByIngredients(array) {
+    //     let masterArray = []
+    //     let workingURL = drinkURL + "filter.php/?i=";
+    //     for(let i = 0; i < array.length; i++) {
+    //         $.get(workingURL + array[i], function(response) {
+    //                 const arrayGen = response.drinks.map(function(v) {return v.strDrink})
+                    
+    //             })
+        
+    //         }
+    // }
+    getIDList("chicken");
+
+    function getIDList(ingredient) {
+        $.get("https://www.themealdb.com/api/json/v1/1/filter.php?i=" + ingredient, function(response) { 
+            const arrayGen = response.meals.map(function(v) {return v.idMeal})
+            console.log(arrayGen);
+            let workingID = arrayGen[Math.floor(Math.random() * arrayGen.length)]
+            $.get("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + workingID, function(response) {
+                console.log(response);
+         }) 
+
+        });
+
     }
-
+    
 })
