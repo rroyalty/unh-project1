5bc1038b9e0bbe72105d05a8aebe27b014ef1862
rroyalty <rroyalty@gmail.com>
2021-01-29 12:46:12 -0500

    Comments and tinkering with promises.

--- assets/scripts/drinkLists.js
+++ assets/scripts/drinkLists.js
@@ -1,23 +1,30 @@
-$(document).ready(function() {
 
-    // List of Ingredients
-    const drinkIngredients = $.get("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list", function(response) {
-        return response;
-    });
 
-    // List of Categories
-    const drinkCategories= $.get("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list", function(response) {
-        return response;
-    });
+// List of Ingredients
+const drinkIngredients = $.get("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list", function(response) {
+    return response;
+})
 
-    // List of Glasses
-    const drinkGlasses= $.get("https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list", function(response) {
-        return response;
-    });
-
-    // List of Alcoholic Filters
-    const drinkAlcoholic= $.get("https://www.thecocktaildb.com/api/json/v1/1/list.php?a=list", function(response) {
-        return response;
-
-    });
+// List of Categories
+const drinkCategories  = $.get("https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list", function(response) {
+    return response;
 });
+
+
+// list of Glasses
+const drinkGlasses = $.get("https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list", function(response) {
+    return response;
+});
+
+// list of Alcoholic 'type'
+const drinkAlcoholic = $.get("https://www.thecocktaildb.com/api/json/v1/1/list.php?a=list", function(response) {
+    return response;
+});
+
+// Example on how to call the promise.
+Promise.resolve(drinkIngredients).then((response) => console.log(response) /* Your function goes here. */ )
+
+
+
+
+
--- assets/scripts/ryanJS.js
+++ assets/scripts/ryanJS.js
@@ -1,53 +1,61 @@
+// Document Ready
 $(document).ready(function() {
+
+    // API Constants
     const drinkAPIkey = "1";
     const drinkURL = "https://www.thecocktaildb.com/api/json/v1/" + drinkAPIkey + "/";
     const filterURL = "filter.php/?i=";
 
-    masterArray = [];
-
+    // Experimental array of ingredients
     let ingredients = ["Vodka", "Gin"];
 
+    // Call getDrinkPromises
     getDrinksPromises(ingredients);
 
+    // getDrinkPromises function
     function getDrinksPromises(array) {
+        
+        // Created an array of promises, each index being the response for one of the ingredients in the array.
         for (let i = 0; i < array.length; i++) {
             array[i] = $.get(drinkURL + filterURL + array[i], function(response) {
                         return response;
                     });
         };
+
+        // Waits for all promises, then generates a list of IDs for the drinks that use the ingredients. Sorts the list by most ingredients found to least.
         Promise.all(array).then((response) => {
             
-            let newObj = {"drinks":[]};
+            // The JSON object that our final list of IDs will sit in.
+            let finalList = {"drinks":[]};
+            
+            // Array used to concat all of the drinks found for each ingredient provided.
             let arrGen = [];
+
+            // Object used to count the number of times that each meal appears in the concat list.
+            let counts = {};
+
+            // Loop to concat the arrays of IDs together.
             for (let i = 0; i < response.length; i++)
                 {
                     arrGen = arrGen.concat(response[i].drinks.map(function(v) {return v.idDrink}));
                 }
-            let counts = {};
 
+            // forEach counts the number of times each ID appears in the list.
             arrGen.forEach(function(x) {
                 counts[x] = (counts[x] || 0)+1;
             })
-            console.log(counts);
-            Object.entries(counts).forEach(e => newObj["drinks"].push({"id":e[0],"count":e[1]}))
-            newObj.drinks.sort(function(a,b) {
+
+            // Converts the resulting counts object into a more easily usable JSON object.
+            Object.entries(counts).forEach(e => finalList["drinks"].push({"id":e[0],"count":e[1]}))
+
+            // Sorts the JSON object by count descending.
+            finalList.drinks.sort(function(a,b) {
                 return b.count - a.count
             })
-            console.log(newObj);
-
+            console.log(finalList);
 
         })
     };
-
-
-
-    // function getDrinks(array) {
-    //     let arrResponse = array.forEach(element => { $.get(drinkURL + filterURL + element, elemenet = function(response) {
-    //         return response;
-    //     } )});
-    //     console.log(arrResponse);
-
-    // }
 
     // getIDList("chicken");
 
