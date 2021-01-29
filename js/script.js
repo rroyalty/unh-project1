fd96b06beff6d8a11bdb8cdcaa84062d24f4c5bb
cmarc <40406039+crrmarchese@users.noreply.github.com>
2021-01-28 21:47:55 -0500

    Add drink dropdown with values - POC

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    
+    
+
+    // const mealAPIkey = "1";
+    // const mealURL = "https://www.themealdb.com/api/json/v1/" + mealAPIkey + "/search.php?i=52772";
+
+    // $.ajax({
+    //     url: mealURL,
+    //     method: "GET"
+    //   }).then(function(response) {
+    //     console.log(response);
+    //   });
+
+});
