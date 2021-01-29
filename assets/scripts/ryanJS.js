1a71a89a332ad476fad505c9a7d0aaf0d263df75
rroyalty <rroyalty@gmail.com>
2021-01-29 11:42:28 -0500

    Fancy function.

--- assets/scripts/ryanJS.js
+++ assets/scripts/ryanJS.js
@@ -1,37 +1,67 @@
 $(document).ready(function() {
     const drinkAPIkey = "1";
     const drinkURL = "https://www.thecocktaildb.com/api/json/v1/" + drinkAPIkey + "/";
+    const filterURL = "filter.php/?i=";
 
-    // let ingredients = ["Vodka", "Gin"];
+    masterArray = [];
 
-    // searchByIngredients(ingredients);
+    let ingredients = ["Vodka", "Gin"];
 
-    // console.log(ingredients);
+    getDrinksPromises(ingredients);
 
-    // function searchByIngredients(array) {
-    //     let masterArray = []
-    //     let workingURL = drinkURL + "filter.php/?i=";
-    //     for(let i = 0; i < array.length; i++) {
-    //         $.get(workingURL + array[i], function(response) {
-    //                 const arrayGen = response.drinks.map(function(v) {return v.strDrink})
-                    
-    //             })
-        
-    //         }
+    function getDrinksPromises(array) {
+        for (let i = 0; i < array.length; i++) {
+            array[i] = $.get(drinkURL + filterURL + array[i], function(response) {
+                        return response;
+                    });
+        };
+        Promise.all(array).then((response) => {
+            
+            let newObj = {"drinks":[]};
+            let arrGen = [];
+            for (let i = 0; i < response.length; i++)
+                {
+                    arrGen = arrGen.concat(response[i].drinks.map(function(v) {return v.idDrink}));
+                }
+            let counts = {};
+
+            arrGen.forEach(function(x) {
+                counts[x] = (counts[x] || 0)+1;
+            })
+            console.log(counts);
+            Object.entries(counts).forEach(e => newObj["drinks"].push({"id":e[0],"count":e[1]}))
+            newObj.drinks.sort(function(a,b) {
+                return b.count - a.count
+            })
+            console.log(newObj);
+
+
+        })
+    };
+
+
+
+    // function getDrinks(array) {
+    //     let arrResponse = array.forEach(element => { $.get(drinkURL + filterURL + element, elemenet = function(response) {
+    //         return response;
+    //     } )});
+    //     console.log(arrResponse);
+
     // }
-    getIDList("chicken");
 
-    function getIDList(ingredient) {
-        $.get("https://www.themealdb.com/api/json/v1/1/filter.php?i=" + ingredient, function(response) { 
-            const arrayGen = response.meals.map(function(v) {return v.idMeal})
-            console.log(arrayGen);
-            let workingID = arrayGen[Math.floor(Math.random() * arrayGen.length)]
-            $.get("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + workingID, function(response) {
-                console.log(response);
-         }) 
+    // getIDList("chicken");
 
-        });
+    // function getIDList(ingredient) {
+    //     $.get("https://www.themealdb.com/api/json/v1/1/filter.php?i=" + ingredient, function(response) { 
+    //         const arrayGen = response.meals.map(function(v) {return v.idMeal})
+    //         console.log(arrayGen);
+    //         let workingID = arrayGen[Math.floor(Math.random() * arrayGen.length)]
+    //         $.get("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + workingID, function(response) {
+    //             console.log(response);
+    //      }) 
 
-    }
+    //     });
+
+    // }
     
 })
